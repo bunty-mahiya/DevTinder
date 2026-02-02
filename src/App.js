9451281f27454c15f626cb26cc289a5d00e4f6ac
bunty <buntymahiya764@gamil.com>
2026-02-02 11:04:37 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 console.log("starting backend code");
 const express=require("express")
 const app=express();
-app.use("/hello", (req,res)=>{
-    res.send("test request received");
+app.get("/abc", (req,res)=>{
+    res.send("hello request received(get)"); 
 })
-app.use((req,res)=>{
-res.send("hello from backend server")
-})
+// app.get("/user/:user", (req,res)=>{
+//     res.send("hello request received(get)");
+//     console.log(req.params); 
+// })
+// app.post("/hello",(req,res)=>{
+//     res.send("hello from backend server(post)")
+// })
+// app.use((req,res)=>{
+//     res.send("hello request received");
+// })
+
 app.listen(3000,()=>{
 console.log("sucessfull  listing on part 7777 ");
 })
