b46acc7a74218469d85b4170159ae89079bde28f
bunty <buntymahiya764@gamil.com>
2026-02-02 16:49:57 +0530

    multiple routes handle for single route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 console.log("starting backend code");
-const express=require("express")
-const app=express();
-app.get("/abc", (req,res)=>{
-    res.send("hello request received(get)"); 
-})
+const express = require("express");
+const app = express();
+//multiple routes handler for single route
+app.get("/abc", [
+  (req, res, next) => {
+    console.log("fisrt handler called");
+    // res.send("hello request received(get) first handler");
+    next();
+  },
+  (req, res, next) => {
+    console.log("2rd handler called");
+    next();
+    // res.send("hello request received(get) 2rd handler");
+  },
+  (req, res, next) => {
+    console.log("3rd handler called");
+    next();
+    // res.send("hello request received(get) 3rd handler");
+  },
+  [
+    (req, res, next) => {
+      console.log("4th handler called");
+      res.send("hello request received(get) 4th handler");
+    },
+  ],
+]);
 // app.get("/user/:user", (req,res)=>{
 //     res.send("hello request received(get)");
-//     console.log(req.params); 
+//     console.log(req.params);
 // })
 // app.post("/hello",(req,res)=>{
 //     res.send("hello from backend server(post)")
@@ -15,6 +36,6 @@ app.get("/abc", (req,res)=>{
 //     res.send("hello request received");
 // })
 
-app.listen(3000,()=>{
-console.log("sucessfull  listing on part 7777 ");
-})
+app.listen(3000, () => {
+  console.log("sucessfull  listing on part 7777 ");
+});
