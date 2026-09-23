8a3c41a32cd5057ae1f13ce0672589ac054020ec
bunty <buntymahiya764@gamil.com>
2026-09-23 12:41:59 +0530

    update something

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -54,6 +54,8 @@ authRouter.post("/login", async (req, res) => {
     res.cookie("token", token,{
   expires: new Date(Date.now() + 24 * 60 * 60 * 1000), // ✅ 1 din baad expire
   httpOnly: true  // JS se cookie access nahi hogi (security ke liye)
+  sameSite: "None",
+   secure: true,
 });
     res.send(user);
   } catch (Error) {
